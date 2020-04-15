dc8043e1cef7967c5a3272770619f4c791304b8f
mdfaizan7 <faizaanazim@gmail.com>
2020-04-16 03:54:47 +0530

    configured the landing page

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,23 +27,20 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div className="body">
+        <div className="container">
+          <main className="">{children}</main>
+        </div>
       </div>
     </>
   )
 }
+
+// ;<footer>
+//   © {new Date().getFullYear()}, Built with
+//   {` `}
+//   <a href="https://www.gatsbyjs.org">Gatsby</a>
+// </footer>
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,58 @@
 import React from "react"
-import { Link } from "gatsby"
-
+// import { Link } from "gatsby"
+import Typical from "react-typical"
+import { Container, Row, Col } from "reactstrap"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import MyImg from "../images/my-img.jpg"
 
 import "../styles/App.scss"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 className="header first">Hi!</h1>
-    <h1 className="header second">I am Faizan</h1>
+const IndexPage = () => {
+  const steps = [
+    "I'm a Student 🎓",
+    1000,
+    "I'm a Programmer 💻",
+    1000,
+    "I'm a ☕ lover",
+    1000,
+    "I'm a pretty fast learner and always intrested in learning new technologies🤓",
+    1000,
+    "I'm a Thinker 💡",
+    1000,
+    "Coding is my passion 🔥",
+    1000,
+    "I think one of my values is the ability to resolve problems",
+    1000,
+    "I'm a Developer ♨️",
+    1000,
+    "I'm a Rapper (wannabe)",
+    1000,
+  ]
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Container className="landing-content">
+        <Row>
+          <Col md={4}>
+            <img src={MyImg} alt="my image" className="image" />
+          </Col>
+          <Col md={8}>
+            {" "}
+            <h1 className="header">
+              <div className="first">Hi,</div>{" "}
+              <div className="second">I am Faizan</div>
+            </h1>
+            <p>
+              <Typical loop={Infinity} wrapper="b" steps={steps} />
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  )
+}
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+//  <Link to="/page-2/">Go to page 2</Link>
 
 export default IndexPage
